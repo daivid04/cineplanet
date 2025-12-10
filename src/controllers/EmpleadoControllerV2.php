54bb639075db8f8f28665a1890c39b040c0ef4b7
awa5000 <eleyvag@unjbg.edu.pe>
2025-12-10 00:34:46 -0500

    fix: update SedeModelFixed to SedeModel reference

--- src/controllers/EmpleadoControllerV2.php
+++ src/controllers/EmpleadoControllerV2.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . "/../models/EmpleadoModel.php";
-require_once __DIR__ . "/../models/SedeModelFixed.php";
+require_once __DIR__ . "/../models/SedeModel.php";
 require_once __DIR__ . "/../models/HorarioModel.php";
 
 class EmpleadoController {
